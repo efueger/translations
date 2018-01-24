db6913c3033fd17b21309c9b975832574ca55f9d
Igor <igorbu4@gmail.com>
2018-01-24 20:56:27 +0700

    implemented UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //add admin
+        $adminPassword = strtoupper(str_random(6));
+        $adminEmail = 'admin@admin.com';
+        User::create(
+            [
+                'name'=>'admin',
+                'email'=>'admin@admin.com',
+                'password'=>bcrypt($adminPassword),
+            ]
+        );
+        $this->command->getOutput()->writeln("Admin's data for login:");
+        $this->command->getOutput()->writeln("-email:<comment>$adminEmail</comment>");
+        $this->command->getOutput()->writeln("-password:<comment>$adminPassword</comment>");
+
     }
 }
