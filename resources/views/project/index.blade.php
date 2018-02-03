3d8310acb17b6e2926c7789e11bbdeba493d14ee
Igor <igorbu4@gmail.com>
2018-02-03 21:36:59 +0700

    create projects index view

---
+++ resources/views/project/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Projects</div>
+
+                    <div class="panel-body">
+
+                        Your projects!<br>
+                        Your projects!<br>
+                        Your projects!<br>
+                        Your projects!<br>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
