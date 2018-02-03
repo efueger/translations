b0de08c8d72db76cb227bf73e21398f98c652615
Igor <igorbu4@gmail.com>
2018-02-03 21:51:19 +0700

    added HasParentModel

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //add admin
+        $this->addAdmin();
+
+
+    }
+
+//    private function addUser()
+//    {
+//        $adminPassword = strtoupper(str_random(6));
+//        $adminEmail = 'admin@admin.com';
+//        User::create(
+//            [
+//                'name' => 'admin',
+//                'email' => 'admin@admin.com',
+//                'password' => bcrypt($adminPassword),
+//                'role_id' => \Bu4ak\Roles\Enum\RoleType::ADMIN,
+//            ]
+//        );
+//        $this->command->line("IsAdmin's data for login:");
+//        $this->command->line("-email:<comment>$adminEmail</comment>");
+//        $this->command->line("-password:<comment>$adminPassword</comment>");
+//    }
+
+    private function addAdmin()
+    {
         $adminPassword = strtoupper(str_random(6));
         $adminEmail = 'admin@admin.com';
         User::create(
