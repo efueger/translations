8c553fd954e9840aceff2d44cb856a362cb0db67
Igor <igorbu4@gmail.com>
2018-02-03 22:04:13 +0700

    added ProjectsTable seeder

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -35,8 +35,43 @@ namespace App{
 
 namespace App{
 /**
+ * App\Client
+ *
+ * @property int $id
+ * @property int $role_id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereRoleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Client whereUpdatedAt($value)
+ */
+	class Client extends \Eloquent {}
+}
+
+namespace App{
+/**
  * App\Project
  *
+ * @property int $id
+ * @property string $name
+ * @property int $user_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereUserId($value)
  */
 	class Project extends \Eloquent {}
 }
